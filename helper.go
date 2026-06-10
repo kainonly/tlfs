b345532bc6e691903a68d1fcc2c0ef553c162060
kain <kainpro@foxmail.com>
2026-06-10 10:08:47 +0800

    新增支付跳转链接生成功能

---
+++ helper.go
@@ -0,0 +1,38 @@
+package tlfs
+
+import (
+	"net/url"
+
+	"github.com/bytedance/sonic"
+)
+
+type PayRedirectURLDto struct {
+	Id        string          `json:"id"`                 // 收款单id
+	SellerId  string          `json:"sellerId"`           // 收款人Id
+	EnterType string          `json:"enterType"`          // 类型
+	Amount    string          `json:"amount"`             // 金额（分为单位）
+	FontsMap  []*FontsMapItem `json:"fontsMap,omitempty"` // 表单数据
+}
+
+type FontsMapItem struct {
+	Id    string `json:"id"`    // 表单项id
+	Value string `json:"value"` // 值
+}
+
+func NewFontsMapItem(id, value string) *FontsMapItem {
+	return &FontsMapItem{Id: id, Value: value}
+}
+
+func (x *Tlfs) PayRedirectURL(dto *PayRedirectURLDto) (_ string, err error) {
+	var fontsMap string
+	if fontsMap, err = sonic.MarshalString(dto.FontsMap); err != nil {
+		return
+	}
+	params := url.Values{}
+	params.Set("id", dto.Id)
+	params.Set("sellerId", dto.SellerId)
+	params.Set("enterType", dto.EnterType)
+	params.Set("amount", dto.Amount)
+	params.Set("fontsMap", fontsMap)
+	return x.Option.BaseURL + "/payment/views/payment.html?" + params.Encode(), nil
+}
